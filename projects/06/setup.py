44221f42504ee3d28242594c40c60c265cab2c14
Daniel Chen <team6612@gmail.com>
2018-05-06 20:45:38 +0800

    Add command line script and setup script

---
+++ projects/06/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyHackAssembler',
+      version='1.0',
+      description='Hack Assembler written in Python',
+      author='Daniel Chen',
+      author_email='daniel@ccns.ncku.edu.tw',
+      packages=['hackassembler'],
+      scripts=['scripts/pyHAsm']
+     )
